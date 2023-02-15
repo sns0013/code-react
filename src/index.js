bfc204c9370dad490749f602d4d1721541d3debf
sns0013 <sns0013@auburn.edu>
2023-02-15 11:15:03 -0600

    Amplify Index

--- src/index.js
+++ src/index.js
@@ -1,29 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-//router
-import { BrowserRouter } from 'react-router-dom'
-
-//store
-
-import { Provider } from 'react-redux';
-//reducer
-import Store from './store'
+import { BrowserRouter } from "react-router-dom";
+import { AmplifyProvider, Authenticator } from "@aws-amplify/ui-react";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <Provider store={Store}>
-            <App />
-        </Provider>
-    </BrowserRouter>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <AmplifyProvider>
+      <Authenticator.Provider>
+        <App />
+      </Authenticator.Provider>
+    </AmplifyProvider> 
+  </BrowserRouter>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
